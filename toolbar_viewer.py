c29f018eca13722f6c5dbc0ef4d567f774c96cf9
Matthew Reimer <matt.reimer@gmail.com>
2016-10-07 14:00:14 -0700

    Got it working without GCD names

--- toolbar_viewer.py
+++ toolbar_viewer.py
@@ -221,7 +221,7 @@ class ToolbarViewer:
             #    removed on close (see self.onClosePlugin method)
             if self.dockwidget == None:
                 # Create the dockwidget (after translation) and keep reference
-                self.dockwidget = ViewerDockWidget()
+                self.dockwidget = ToolbarDockWidget()
 
             # connect to provide cleanup on closing of dockwidget
             self.dockwidget.closingPlugin.connect(self.onClosePlugin)
