093ce56e99792901b81daa09cf6a9afc472fb19c
Matthew Reimer <matt.reimer@gmail.com>
2017-03-22 11:57:00 -0700

    backcopy gets implemented

---
+++ scripts/backcopy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+##############################################
+# BE REALLY SURE YOU WANT TO RUN THIS
+##############################################
+# This file is used by Matt Reimer:
+#
+# When I debug I don't keep my source code in .qgis2/python/plugins
+# I store it somewhere else and use pb_tool to deploy it to .qgis2/python/plugins
+# That means when I debug, the breakpoint is hitting the .qgis2 version of the code
+#
+# If I make changes there I might want to copy those changes back so I can commit them
+#
+##############################################
+
+QGSPATH=~/.qgis2/python/plugins/RiverscapesToolbar
+
+cp $QGSPATH/*.py ../
+cp -fr $QGSPATH/lib ../lib
