e5f2bb6b3d1f02bd49f80c0460b4e54040a99f82
Matthew Reimer <matt.reimer@gmail.com>
2017-06-14 16:37:38 -0700

    deploy script upgrade

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,19 @@
 NEWVERSION=$1
 
 sed -i -- "s/^version=.*/version=${NEWVERSION}/g" metadata.txt
+
 git checkout master
 git add metadata.txt;
 git commit -m "Upgrading to version: $NEWVERSION"
 git tag $NEWVERSION
 git push --all
+
+# Now make a zip and upload it
 pb_tool zip
-aws s3 cp RiverscapesToolbar.zip s3://qgis.northarrowresearch.com/plugins/riverscapestoolbar/riverscapestoolbar-$NEWVERSION.zip --profile matt
+aws s3 cp RiverscapesToolbar.zip s3://qgis.northarrowresearch.com/plugins/riverscapestoolbar/riverscapestoolbar-$NEWVERSION.zip
+rm RiverscapesToolbar.zip
+
+# Now let dev catchup
+git checkout dev
+git merge master
+git push
