0322507b235b1aed04ae3fd25fa2ac606ab585b3
jesselangdon <jesselangdon@gmail.com>
2017-08-16 12:21:34 -0700

    Revised symbolizers for solar and EC.

--- RiverscapesToolbar/symbology/symbolizers/raster_solar.py
+++ RiverscapesToolbar/symbology/symbolizers/raster_solar.py
@@ -8,17 +8,23 @@ class RasterSymbolizer():
     def symbolize(self):
         self.colorramptype = QgsColorRampShader.INTERPOLATED
         # Now you have a self.layer object and self.stats you can work with
-        self.opacity = 0.4
+        self.opacity = 0.6
 
         colDic = {
-            'blue': '#1977EF',
-            'red': '#EA4A39'
+            'Low':'#0000FF',
+            'Medium':'#FFFF00',
+            'High':'#FF0000'
         }
 
-        lo = self.stats.minimumValue
-        hi = self.stats.maximumValue
+        min = self.stats.minimumValue
+        max = self.stats.maximumValue
+
+        # calculate median
+        range = max - min
+        med = range//2
 
         self.colLst = [
-            [lo, QColor(colDic['blue']), str(lo)],
-            [hi, QColor(colDic['red']), str(hi)]
+            [min, QColor(colDic['Low']), str(min)],
+            [med, QColor(colDic['Medium']), str(med)],
+            [max, QColor(colDic['High']), str(max)]
         ]
